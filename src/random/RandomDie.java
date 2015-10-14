6d9fa11858fdc863872410056b514ca658a6db69
116060088 <116060088@ECH-STU-05-21C.SSFSD.LOCAL>
2015-10-14 08:49:51 -0700

    complete!

--- src/random/RandomDie.java
+++ src/random/RandomDie.java
@@ -13,4 +13,17 @@ public class RandomDie {
 		for(int i = 0; i < m; i++) result += nSidedDie(n);
 		return result;
 	}
+	public static double randomBoundedDouble(double a, double b) {
+		return a+((b-a)*randomFromZeroToOne());
+	}
+	public static void main(String[] args) {
+		System.out.println("From 1d6: you rolled a "+nSidedDie(6)+'!');
+		System.out.println("From 1d20: you rolled a "+nSidedDie(20)+'!');
+		System.out.println("Random number between 0.0 and 1.0: "+randomFromZeroToOne());
+		System.out.println("Random number between 5.0 and 6.0: "
+				+randomBoundedDouble(5.0,6.0));
+		System.out.println("Random number between 2.0 and 5.0: "
+				+randomBoundedDouble(2.0,5.0));
+		System.out.println("From 2d6: you rolled a "+mDn(2,6)+'!');
+	}
 }
